41ea7cdb000e243a7b9e5a64ae0596a9025714fb
Ajay Singh Parmar <ajays.parmar04@gmail.com>
2014-07-02 19:43:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>City Portal</title>
+<meta name="keywords" content="city portal, 3 columns, free css templates, website templates, white color" />
+<meta name="description" content="City Portal is a 3-column free website template for everyone" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+
+    
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="WOW Slider, jquery slider, jquery slider" />
+    <meta name="description" content="WOWSlider created with WOW Slider, a free wizard program that helps you easily generate beautiful web slideshow" />
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+    <script type="text/javascript" src="engine1/jquery.js"></script>
+    <!-- End WOWSlider.com HEAD section -->
+
+</head>
+<body>
+
+<div id="templatmeo_wrapper">
+
+    <div id="templatemo_header">
+    
+        <div id="site_title">
+            <h1><a href="index.php" target="_parent">
+                <img src="7.jpg" alt="City Portal" />
+                <span>Welcome to SecureCity</span>
+            </a></h1>
+        </div> <!-- end of site_title -->
+        
+        <div id="header_right">
+        
+            <ul id="header_button">
+                <li><a href="login.php"><img src="8.jpg" alt="home" /></a></li>
+            
+            </ul>
+            
+            <div class="cleaner"></div>
+            
+            <form action="#" method="get">
+                <input type="text" placeholder="Search here For secure City" name="q" size="10" id="searchfield" title="searchfield" onfocus="clearText(this)" onblur="clearText(this)" />
+                <input type="submit" name="Search" value="Go" alt="Search" id="searchbutton" title="Search" />
+            </form>
+        
+        </div>
+        
+    </div> <!-- end of templatemo_header -->
+    
+    
+    
+  <div id="wowslider-container1">
+    <div class="ws_images"><ul>
+<li><img src="data1/images/6.jpg" alt="6" title="6" id="wows1_0"/></li>
+<li><img src="data1/images/5.png" alt="5" title="5" id="wows1_1"/></li>
+<li><img src="data1/images/4.jpg" alt="4" title="4" id="wows1_2"/></li>
+<li><img src="data1/images/3.jpg" alt="3" title="3" id="wows1_3"/></li>
+<li><img src="data1/images/2.png" alt="2" title="2" id="wows1_4"/></li>
+<li><img src="data1/images/1.jpg" alt="1" title="1" id="wows1_5"/></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title="6"><img src="data1/tooltips/6.jpg" alt="6"/>1</a>
+<a href="#" title="5"><img src="data1/tooltips/5.png" alt="5"/>2</a>
+<a href="#" title="4"><img src="data1/tooltips/4.jpg" alt="4"/>3</a>
+<a href="#" title="3"><img src="data1/tooltips/3.jpg" alt="3"/>4</a>
+<a href="#" title="2"><img src="data1/tooltips/2.png" alt="2"/>5</a>
+<a href="#" title="1"><img src="data1/tooltips/1.jpg" alt="1"/>6</a>
+</div></div>
+<span class="wsl">jquery slider</a>Copyright</span>
+    <div class="ws_shadow"></div>
+    </div>
+    <script type="text/javascript" src="engine1/wowslider.js"></script>
+    <script type="text/javascript" src="engine1/script.js"></script>
+    <!-- End WOWSlider.com BODY section -->
+
+    
+    <div id="templatemo_content_wrapper">
+    
+    	<div id="templatemo_left_sidebar">
+        
+        	<div class="templatemo_box">
+            	<h2><span></span>Categories</h2>
+                
+                <div class="body">
+                    <ul class="side_menu">
+                        <li><a href="login.php">Complaint Box</a></li>
+                        <li><a href="#">Police stations</a></li>
+                        <li><a href="#">Children & youth</a></li>
+                        <li><a href="#">Wanted</a></li>
+                        <li><a href="#">City cementery</a></li>
+                        <li><a href="#">Community service</a></li>
+                        <li><a href="#">Safety</a></li>
+                        <li><a href="#">Charitable Donations </a></li>              
+                    </ul>
+                </div>
+            
+            	<div class="box_bottom"><span></span></div>
+            </div>
+            
+            <div class="templatemo_box">
+            	<h2><span></span>Useful News</h2>
+                
+                <div class="body">
+                <ul class="side_menu">
+                <marquee  width=170 height=200 direction=up scrollamount="5">
+          			
+                        <li><a href="#">Today's CIA</a></li>
+                        <li><a href="#">Career Opportunities</a></li>
+                        <li><a href="#">Headquarters Tour</a></li>
+                        <li><a href="#">Military Transition</a></li>
+                        <li><a href="#">Press Releases & Statements</a></li>
+                        <li><a href="#">Speeches & Testimony</a></li>
+                        <li><a href="#">Diversions & Information</a></li>
+                        <li><a href="#">Human Resources</a></li>
+					
+               </marquee></ul>
+                </div>
+            
+            	<div class="box_bottom"><span></span></div>
+            </div>
+        
+        </div> <!-- end of left_sidebar -->
+        
+        <div id="templatemo_content">
+        
+			<div class="templatemo_box">
+            	<h2><span></span>Our Mission</h2>
+                
+                <div class="body">
+                    
+                    <p>Preempt threats and further INDIAN National Security objectives by collecting intelligence that matters, prod
+                    ucing objective all-source analysis, conducting effective covert action as directed by the President, and sa
+                    feguarding the secrets that help keep our Nation safe.</p>
+                  
+              </div>
+            
+           	  <div class="box_bottom"><span></span></div>
+            </div>
+            
+            <div class="templatemo_box">
+            	<h2><span></span>Popular Places</h2>
+                
+                <div class="body">
+                    
+                    <div class="news_box">
+                        <a href="#"><img class="news_image" src="images/templatemo_image_06.jpg" alt="image" /></a>
+                   	  <h3><a href="#">Environmental Services calendar</a></h3>
+                        <p>Check the Environmental Services calendar for recycling and waste reduction events, programs and services for residents
+                        , businesses and the military.</p>
+                        
+                      <div class="cleaner"></div>
+                    </div>
+                
+              <div class="news_box">
+                    <a href="#"><img class="news_image" src="images/templatemo_image_07.jpg" alt="image" /></a>
+                	<h3><a href="#">Noida's Speakers Bureau</a></h3>
+                    <p>The City of San Diego’s Speakers Bureau is available to schools and community partnerships on a broad range of topics and areas of study</p>
+                    <div class="cleaner"></div>
+                </div>
+                
+              <div class="news_box">
+                    <a href="#"><img class="news_image" src="images/templatemo_image_08.jpg" alt="image" /></a>
+               	<h3><a href="#">water conservation programs </a></h3>
+                    <p>The City's water conservation programs will help you save money on your water bill and conserve water, our most valuable resource.</p>
+                <div class="cleaner"></div>
+                </div>
+                
+                <div class="more float_r"><a href="#">View All</a></div>
+                
+                <div class="cleaner"></div>
+                  
+                </div>
+            
+            	<div class="box_bottom"><span></span></div>
+            </div>
+        	
+        </div> <!-- end of templatemo_content -->
+        
+       	<div id="templatemo_right_sidebar">
+         
+        	<div class="templatemo_box">
+            	<h2><span></span>Emergency Complaint Box</h2>
+                <form method="post">
+                <div class="body">
+                    
+                   
+<table>
+<tr><td>Your User ID :</td><td><input type="text" name="sno" placeholder="user id" size="9"/></td></tr>
+<tr>
+<td>Date :</td><td><input type="text" name="date" size="9"/></td>
+</tr>
+
+<tr><td>Emergency :</td>
+<td>
+<select name="emergency" size="auto" >
+<option value="null">--emergency--</option>
+<option value="murder">Murder</option>
+<option value="robbery">Robbery</option>
+<option value="accident">Accident</option>
+
+</select>
+</td>
+</tr>
+
+
+<tr>
+<td><strong>Complaint BOX :</strong></td>
+
+<td><textarea placeholder="type here !!" name="description" ></textarea>
+</td></tr>
+<tr><td><input type="submit" name="send" value="Add Complaint"/></td></tr>
+</table>
+
+                    
+     </div></form>
+            
+            	<div class="box_bottom"><span></span></div>
+            </div>
+            
+            <div class="sidebar_box">
+            	<a href="#" target="_parent"><img src="images/nightlife.png" alt="Night Life" /></a>            </div>
+            
+            <div class="sidebar_box">
+            	<a href="#" target="_parent"><img src="images/city-zoo.png" alt="City Zoo" /></a>            </div>
+        
+      </div> <!-- end of right_sidebar -->
+        
+        <div class="cleaner"></div>
+    
+    </div> <!-- end of templatemo_content_wrapper -->
+
+</div> <!-- end of templatemo_wrapper -->
+
+<div id="templatemo_footer_wrapper">
+
+	<div id="templatemo_footer">
+
+        <ul class="footer_menu">
+        	<li><a href="#">Homepage</a></li>
+            <li><a href="#">City Hall</a></li>
+            <li><a href="#">Bussiness</a></li>
+            <li><a href="#">Information</a></li>
+            <li><a href="#">Technology</a></li>
+            <li><a href="admin.php">Admin Login</a></li>
+            <li class="last_menu"><a href="#">Contact</a></li>
+        </ul>
+    
+        Copyright © 2048 <a href="#">ass_hall.org</a> | 
+    <a href="#" target="_parent">city without crime</a> by <a href="#" target="_parent">Developers</a></div> 
+	<!-- end of footer -->
+</div> <!-- end of templatmeo_footer_wrapper -->
+</html>
+
+<?php
+
+include("config.php");
+    
+   
+    
+    if(isset($_REQUEST['send'])){
+        
+         $sno= $_POST['sno'];
+    $date= $_POST['date'];
+    $emergency= $_POST['emergency'];
+    $desc= $_POST['description'];
+        
+        if(!empty($_POST['sno'])){
+            
+            $qry= "INSERT INTO emergency values($sno,'$desc','$date','$emergency') ";
+            $result=mysql_query($qry);
+            
+            
+            echo "<script>alert('Your Complaint is Successfully Registered')</script>";
+           
+            
+        }
+        else{
+             echo "Your haven't enter your User ID!!";  
+             }
+
+}
+
+?>
